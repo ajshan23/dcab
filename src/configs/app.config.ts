0b5899428778ebe71c4d9c40da33bf1ab27ec63f
ajshan23 <ajmalshahan23@gmail.com>
2025-08-05 23:43:27 +0530

    changed url

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -8,7 +8,7 @@ export type AppConfig = {
 }
 
 const appConfig: AppConfig = {
-    apiPrefix: 'http://localhost:3001/api',
+    apiPrefix: 'https://dca.ksa-sc.com/api',
     authenticatedEntryPath: '/home',
     unAuthenticatedEntryPath: '/sign-in',
     tourPath: '/',
@@ -17,4 +17,4 @@ const appConfig: AppConfig = {
 }
 
 export default appConfig
-//https://dca.ksa-sc.com
+//
